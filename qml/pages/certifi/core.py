445ec6a644e74c63e92b05781845175c83791f9c
BirdZhang <0312birdzhang@gmail.com>
2019-11-05 15:49:24 +0800

    update

--- qml/pages/certifi/core.py
+++ qml/pages/certifi/core.py
@@ -9,12 +9,8 @@ This module returns the installation location of cacert.pem.
 import os
 
 def where():
-#    f = os.path.dirname(__file__)
-#   hard code
-    f = "/usr/share/"
-
-    return os.path.join(f,'harbour-sailfishclub','qml', 'cacert.pem')
-
+    f = os.path.dirname(__file__)
+    return os.path.join(f, 'cacert.pem')
 
 if __name__ == '__main__':
     print(where())
--- rpm/harbour-kuaidi.spec
+++ rpm/harbour-kuaidi.spec
@@ -13,7 +13,7 @@ Name:       harbour-kuaidi
 %{!?qtc_make:%define qtc_make make}
 %{?qtc_builddir:%define _builddir %qtc_builddir}
 Summary:    Chinese express check application
-Version:    1.3
+Version:    1.4
 Release:    1
 Group:      Qt/Qt
 License:    LICENSE
